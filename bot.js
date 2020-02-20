0b07cf0773221abef916f3b62ab8a6d80ffd984e
BuildTools <unconfigured@null.spigotmc.org>
2020-02-20 14:33:26 -0500

    Test

--- bot.js
+++ bot.js
@@ -92,3 +92,39 @@ app.use('/viatges', router);
 app.listen(process.env.PORT || 3000 ,function(){
     console.log("up and running on port "+process.env.PORT);
 });
+
+const fetch = require("node-fetch");
+
+const wakeUpDyno = (url, interval = 25, callback) => {
+    const milliseconds = interval * 60000;
+    const url = "https://github.com/kylef7/elon-heroku-bot";
+    setTimeout(() => {
+
+        try {
+            console.log(`setTimeout called.`);
+            // HTTP GET request to the dyno's url
+            fetch(url).then(() => console.log(`Fetching ${url}.`));
+        }
+        catch (err) { // catch fetch errors
+            console.log(`Error fetching ${url}: ${err.message} 
+            Will try again in ${interval} minutes...`);
+        }
+        finally {
+
+            try {
+                callback(); // execute callback, if passed
+            }
+            catch (e) { // catch callback error
+                callback ? console.log("Callback failed: ", e.message) : null;
+            }
+            finally {
+                // do it all again
+                return wakeUpDyno(url, interval, callback);
+            }
+
+        }
+
+    }, milliseconds);
+};
+
+module.exports = wakeUpDyno;
