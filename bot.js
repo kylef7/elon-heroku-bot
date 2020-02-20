69bb9640d208269350262642e134f2e15296f5ca
BuildTools <unconfigured@null.spigotmc.org>
2020-02-20 01:20:07 -0500

    Test

--- bot.js
+++ bot.js
@@ -82,5 +82,13 @@ setInterval(() => {
     });
 },1000);//RUNS EVERY 1 SECONDS
 
-var port_number = process.env.PORT || 3000;
-app.listen(port_number);
+var express = require('express');
+var app = express();
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());
+var router = require("./router");
+app.use('/viatges', router);
+
+app.listen(process.env.PORT || 3000 ,function(){
+    console.log("up and running on port "+process.env.PORT);
+});
